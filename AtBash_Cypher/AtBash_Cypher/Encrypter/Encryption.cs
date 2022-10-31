0ff655786026f9a0f3a94bcf9e60c4fe02415854
krisholl <krisholl2022@gmail.com>
2022-10-31 13:08:33 -0400

    Finished first round of this problem. works in O(n)
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ AtBash_Cypher/AtBash_Cypher/Encrypter/Encryption.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtBash_Cypher.Encrypter
+{
+    public class Encryption
+    {
+        public Encryption()
+        {
+
+        }
+        private static string Lower { get; } = "abcdefghijklmnopqrstuvwxyz";
+        private static string ReverseLower { get; } = string.Join("", Lower.Reverse());
+
+        private static string Upper { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static string ReverseUpper { get; } = string.Join("", Upper.Reverse());
+
+        public string Encrypt(string regular)
+        {
+            if(regular == "")
+            {
+                return "Nothing given";
+            }
+            string result = "";
+            foreach(char c in regular)
+            {
+                if (Lower.IndexOf(c) != -1)
+                {
+                    result += ReverseLower[Lower.IndexOf(c)];
+                }
+                else if(Upper.IndexOf(c)!=-1)
+                {
+                    result += ReverseUpper[Upper.IndexOf(c)];
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+
+        public string Decrypt(string cryptic)
+        {
+            if (cryptic == "")
+            {
+                return "Nothing given";
+            }
+            string result = "";
+            foreach (char c in cryptic)
+            {
+                if(Lower.IndexOf(c) != -1)
+                {
+                    result += ReverseLower[Lower.IndexOf(c)];
+                }
+                else if(Upper.IndexOf(c) != -1)
+                {
+                    result += ReverseUpper[Upper.IndexOf(c)];
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AtBash_Cypher/AtBash_Cypher/Program.cs
@@ -0,0 +1,12 @@
+﻿using AtBash_Cypher.Encrypter;
+
+Encryption v = new Encryption();
+Console.WriteLine("Enter a word to Encrypt");
+
+string wordToEncrypt = Console.ReadLine()!;
+
+Console.WriteLine($"Encrypted Word  = {v.Encrypt(wordToEncrypt)}");
+
+Console.WriteLine("Enter a word to decrypt");
+string wordToDecrypt = Console.ReadLine()!;
+Console.WriteLine($"Decrypted Word  = {v.Decrypt(wordToDecrypt)}");
